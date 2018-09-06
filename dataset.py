d79600c390381e2478c0cd3d7e8993b4e9dcdc3a
Fei Li <feili@Feis-MBP.lan>
2018-09-06 07:49:28 -0400

    09/06/2018

---
+++ dataset.py
@@ -0,0 +1,4 @@
+
+
+def loadData(file_path):
+    pass
---
+++ main.py
@@ -0,0 +1,15 @@
+
+import random
+import numpy as np
+import torch
+
+from options import opt
+
+
+print(opt)
+
+if opt.random_seed != 0:
+    random.seed(opt.random_seed)
+    np.random.seed(opt.random_seed)
+    torch.manual_seed(opt.random_seed)
+    torch.cuda.manual_seed_all(opt.random_seed)
